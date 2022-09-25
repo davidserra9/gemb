8db7dd430fe502551f82e89c9b6d8298dabbb629
David Serrano <99d.serrano@gmail.com>
2022-09-25 23:59:09 +0200

    CLIP + MLP

--- utils/model.py
+++ utils/model.py
@@ -8,12 +8,21 @@ import matplotlib.pyplot as plt
 from utils.dataset import TripletGUIE, get_validation_augmentations
 
 class CLIP_MLP(nn.Module):
-    def __init__(self, clip_model='ViT-g-14'):
+    def __init__(self, clip_model='ViT-g-14', pretrained='laion2b_s12b_b42k'):
         super().__init__()
-        self.backbone_clip, _, preprocess = open_clip.create_model_and_transforms(model_name=clip_model,
-                                                                                  pretrained='openai')
-        #TODO MLP (LA SALIDA DEL CLIP TIENE 512 PARAMS - torch.Size([1, 512]))
         self.size = 224
+        self.backbone_clip, _, preprocess = open_clip.create_model_and_transforms(model_name=clip_model,
+                                                                                  pretrained=pretrained)
+        for param in self.backbone_clip.parameters():
+            param.requires_grad = False
+
+        self.mlp = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64)
+        )
     def forward(self, img):
 
         x = T.functional.resize(img, size=[self.size, self.size])
@@ -22,18 +31,18 @@ class CLIP_MLP(nn.Module):
                                    mean=[0.48145466, 0.4578275, 0.40821073],
                                    std=[0.26862954, 0.26130258, 0.27577711])
 
-        out = self.backbone_clip.encode_image(x)
-        return out
+        x = self.backbone_clip.encode_image(x)
+        x = self.mlp(x)
+        return x
 
 if __name__ == "__main__":
-    model = CLIP_MLP(clip_model='ViT-B-32')
+    model = CLIP_MLP(clip_model='ViT-B-32', pretrained='openai')
     dataset = TripletGUIE(root="/home/david/Workspace/gemb/data",
                           train=True,
                           places=False,
                           apparel=True)
 
     (img1, img2, img3), (l1, l2, l3) = dataset[0]
-
 
     out = model(img1.unsqueeze(0))
     print(out.shape)
